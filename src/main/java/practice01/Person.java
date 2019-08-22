b2f9a8a5d9cb291ef11e6a521a53c2ef9abe3c6a
zhangyiran01 <54269459+zhangyiran01@users.noreply.github.com>
2019-08-22 21:58:37 +0800

    Update Person.java

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,29 @@
 package practice01;
 
 public class Person {
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String introduce() {
+		return String.format("My name is %s. I am %d years old.", this.name, this.age);
+	}
+	
 }
